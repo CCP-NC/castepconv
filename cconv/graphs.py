22abba7c7d179e04d6500acee940d1d8f7ff0986
ssturniolo <simone.sturniolo@gmail.com>
2017-08-22 12:47:30 +0100

    Changed graphs to using rescaled deviations

--- cconv/graphs.py
+++ cconv/graphs.py
@@ -1,117 +1,176 @@
 import os
+from math import log10, floor
 
 _x_types = {'cut': 'Cutoff (eV)', 'kpn': 'k-points', 'fgm': 'Fine Gmax (eV)'}
-_y_types = {'for': 'Maximum force (ev/Ang)', 'str': 'Maximum stress (GPa)'}
+_y_types = {'nrg': 'Final energy (eV/Atom)',
+            'for': 'Maximum force ({scale} ev/Ang)',
+            'str': 'Maximum stress ({scale} GPa)'}
 
-def gp_graph(seedname, cnvstr=False):
+
+def find_scale(data):
+    # Find common scales for the residuals of the given data
+    scales = {}
 
     for x in _x_types:
-        for y in _y_types:
-            if y == 'str' and not cnvstr:
-                continue
 
-            # Check if source .dat file exists
-            if not os.path.isfile(os.path.join(seedname + '_' + x  + '_conv.dat')):
-                continue
-            out_file = open(seedname + '_' + x  + ('_str' if y == 'str' else '') + '_conv.gp', 'w')
+        if data[x]['range'][0] is None:
+            continue
 
-            out_file.write('set xlabel "' + _x_types[x] + '"\n')
-            out_file.write('set ylabel "Final energy (eV/atom)"\n')
-            out_file.write('set y2label "' + _y_types[y] + '"\n')
-            out_file.write('set ytics nomirror\n')
-            out_file.write('set y2tics\n')
-            out_file.write('plot "' + seedname + '_' + x + '_conv.dat" using 1:2 with linespoints pt 7 lc 1 ti "Final energy (eV)",')
-            out_file.write('"" using 1:' + {'for': '3', 'str': '4'}[y] +
-            ' with linespoints pt 7 lc ' + {'for': '2', 'str': '3'}[y] + ' axes x1y2 ti "' + _y_types[y] + '"\n')
-            out_file.write('pause -1 "Hit return to continue"\n')
+        # Maximum deviations from final value
+        Ms = {y: max([abs(d-data[x][y][-1]) for d in data[x][y]])
+              for y in _y_types}
+        # Proposed scaling factors
+        scales[x] = {y: 10**floor(log10(Ms['nrg']/m)) if m > 0 else 1
+                     for y, m in Ms.iteritems()}
 
-            out_file.close()
+    return scales
+
+
+def gp_graph(seedname, data, cnvstr=False):
+
+    # data needs to be a dict formatted as: {'cut':  {'range': [...], 'nrg':
+    # [...], 'for': [...] etc.}}
+
+    scales = find_scale(data)
+
+    plot_details = {'nrg': {
+        'col': '3',
+        'lc': '1',
+    },
+        'for': {
+        'col': '4',
+        'lc': '2',
+    },
+        'str': {
+        'col': '5',
+        'lc': '3',
+    }
+    }
+
+    for x, xlabel in _x_types.iteritems():
+
+        # Check if source .dat file exists
+        file_seed = '{seedname}_{x}_conv'.format(seedname=seedname, x=x)
+        if not os.path.isfile(file_seed + '.dat'):
+            continue
+        out_file = open(file_seed + '.gp', 'w')
+
+        out_file.write('set xlabel "{xlabel}"\n'.format(xlabel=xlabel))
+        out_file.write('set ylabel "Deviation"\n')
+        out_file.write('set xtics nomirror\n')
+        out_file.write('set ytics nomirror\n')
+        out_file.write('plot ')
+        for y, legend in _y_types.iteritems():
+            scale = scales[x][y]
+            lsc = '10^{{{0}}}'.format(-int(log10(scale)))
+            out_file.write(('"{file_seed}.dat" using 1:((${col}-({ref}))*{scale})'
+                            ' with linespoints pt 7 lc {lc} '
+                            'ti "{legend}",').format(file_seed=file_seed,
+                                                     scale=scale,
+                                                      legend=legend.format(scale=lsc),
+                                                      ref=data[x][y][-1],
+                                                      **plot_details[y])
+                           )
+        out_file.write(' 0 lt 0 lc 0 notitle\n')
+
+        out_file.write('pause -1 "Hit return to continue"\n')
+        out_file.close()
+
 
 def agr_graph(seedname, data, cnvstr=False):
 
-    # data needs to be a dict formatted as: {'cut':  {'range': [...], 'nrg': [...], 'for': [...] etc.}}
+    # data needs to be a dict formatted as: {'cut':  {'range': [...], 'nrg':
+    # [...], 'for': [...] etc.}}
 
+    scales = find_scale(data)
 
-    for x in _x_types:
+    plot_details = {'nrg': {
+        'set': '0',
+        'lc': '1',
+    },
+        'for': {
+        'set': '1',
+        'lc': '2',
+    },
+        'str': {
+        'set': '2',
+        'lc': '3',
+    }
+    }
+
+    for x, xname in _x_types.iteritems():
 
         if x == 'fgm' and len(data[x]['nrg']) == 0:
             # No fine grid convergence was performed
             continue
 
+        file_seed = '{seedname}_{x}_conv'.format(seedname=seedname, x=x)
+        out_file = open(file_seed + '.agr', 'w')
+
+        x1 = data[x]['range']
+        x1rng = (min(x1), max(x1))
+
+        # Set up the graphics
+
+        out_file.write('@version 50123\n')
+        out_file.write(('@title "{seedname} - Convergence '
+                        'vs {x}"\n').format(seedname=seedname, x=xname))
+        out_file.write('@g0 on\n@g0 hidden false\n@with g0\n')
+
+        # Find the optimal size for the world
+        y1rng = [0,0]
         for y in _y_types:
+            y1 = data[x][y]
+            s = scales[x][y]
+            y1rng[0] = min(y1rng[0], min([(p-y1[-1])*s for p in y1]))
+            y1rng[1] = max(y1rng[1], max([(p-y1[-1])*s for p in y1]))
+
+        y1span = y1rng[1]-y1rng[0]
+        y1rng = [y1rng[0]-0.1*y1span, y1rng[1]+0.1*y1span]
+
+        out_file.write('@world ' + str(x1rng[0]) + ',' + str(
+            y1rng[0]) + ',' + str(x1rng[1]) + ',' + str(y1rng[1]) + '\n')
+
+        out_file.write(
+            '@    view 0.150000, 0.150000, 1.150000, 0.850000\n')
+        out_file.write('@    xaxis label "' + _x_types[x] + '"\n')
+        out_file.write('@    xaxis tick major ' +
+                       str(data[x]['step']) + '\n')
+        out_file.write('@    xaxis offset 0.0, 1.0\n')
+
+        for y, legend in _y_types.iteritems():
             if y == 'str' and not cnvstr:
                 continue
 
             # Cutoff vs energy and forces
 
-            out_file = open(seedname + '_' + x + ('_str' if y == 'str' else '') + '_conv.agr', 'w')
 
-            x1 = data[x]['range']
-            y1 = data[x]['nrg']
-            y2 = data[x][y]
+            y1 = data[x][y]
+            set_header = '@    s{set}'.format(**plot_details[y])
+            lsc = legend.format(scale='10\S{0}\N'.format(-int(log10(scales[x][y]))))
 
-            x1rng = (min(x1), max(x1))
-            y1rng = (min(y1)-0.1*(max(y1)-min(y1)), max(y1)+0.1*(max(y1)-min(y1)))
-            y2rng = (min(y2)-0.1*(max(y2)-min(y2)), max(y2)+0.1*(max(y2)-min(y2)))
+            out_file.write("""
+@    yaxis label "Final energy (eV/atom)"
+@    yaxis tick major {ytick}
+@    yaxis offset 0.0, 1.0
+{set_header} hidden false
+{set_header} on
+{set_header} legend "{legend}"
+{set_header} line color {lc}
+{set_header} symbol 1
+{set_header} symbol color {lc}
+{set_header} symbol fill color {lc}
+{set_header} symbol fill pattern 1
+""".format(set_header=set_header, legend=lsc, 
+           ytick=0.25*10**(floor(log10((y1rng[1]-y1rng[0])))), **plot_details[y]))
 
-            # Set up the graphics
+        # Input the actual data
 
-            out_file.write('@version 50123\n')
-            out_file.write('@title "' + seedname + ' - Energy and ' + {'for': 'forces', 'str': 'stresses'}[y] + 'vs ' +
-            {'cut': 'cutoff', 'kpn': 'k-points', 'fgm': 'fine Gmax'}[x] + '"\n')
+        for y in _y_types:
 
-            out_file.write('@g0 on\n@g0 hidden false\n@with g0\n')
-            out_file.write('@world ' + str(x1rng[0]) + ',' + str(y1rng[0]) + ',' + str(x1rng[1]) + ',' + str(y1rng[1]) + '\n')
-            out_file.write('@    view 0.150000, 0.150000, 1.150000, 0.850000\n')
-            out_file.write('@    xaxis label "' + _x_types[x] + '"\n')
-            out_file.write('@    xaxis tick major ' + str(data[x]['step']) + '\n')
-            out_file.write('@    xaxis offset 0.0, 1.0\n')
-            out_file.write('@    yaxis label "Final energy (eV/atom)"\n')
-            out_file.write('@    yaxis tick major ' + str((y1rng[1]-y1rng[0])/8.0) + '\n')
-            out_file.write('@    yaxis offset 0.0, 1.0\n')
-            out_file.write('@    s0 hidden false\n@    s0 on\n')
-            out_file.write('@    s0 legend "Final energy"\n')
-            out_file.write('@    s0 line color 1\n')
-            out_file.write('@    s0 symbol 1\n')
-            out_file.write('@    s0 symbol size 0.7\n')
-            out_file.write('@    s0 symbol color 1\n')
-            out_file.write('@    s0 symbol fill color 1\n')
-            out_file.write('@    s0 symbol fill pattern 1\n')
-
-            out_file.write('@g1 on\n@g1 hidden false\n@with g1\n')
-            out_file.write('@world ' + str(x1rng[0]) + ',' + str(y2rng[0]) + ',' + str(x1rng[1]) + ',' + str(y2rng[1]) + '\n')
-            out_file.write('@    view 0.150000, 0.150000, 1.150000, 0.850000\n')
-            out_file.write('@    xaxis label ""\n')
-            out_file.write('@    xaxis tick off\n')
-            out_file.write('@    xaxis tick major ' + str(data[x]['step']) + '\n')
-            out_file.write('@    yaxis label "' + _y_types[y] + '"\n')
-            out_file.write('@    yaxis tick major ' + str((y2rng[1]-y2rng[0])/8.0) + '\n')
-            out_file.write('@    yaxis ticklabel format exponential\n')
-            out_file.write('@    yaxis ticklabel prec 1\n')
-            out_file.write('@    yaxis offset 1.0, 0.0\n')
-            out_file.write('@    yaxis label place opposite\n')
-            out_file.write('@    yaxis tick place opposite\n')
-            out_file.write('@    yaxis ticklabel place opposite\n')
-            out_file.write('@    s0 hidden false\n@    s0 on\n')
-            out_file.write('@    s0 legend "' + _y_types[y] + '"\n')
-            out_file.write('@    s0 line color 2\n')
-            out_file.write('@    s0 symbol 1\n')
-            out_file.write('@    s0 symbol size 0.7\n')
-            out_file.write('@    s0 symbol color 2\n')
-            out_file.write('@    s0 symbol fill color 2\n')
-            out_file.write('@    s0 symbol fill pattern 1\n')
-
-            # Input the actual data
-
-            out_file.write('@target G0.S0\n@type xy\n')
+            out_file.write('@target G0.S{0}\n@type xy\n'.format(plot_details[y]['set']))
             for i, c in enumerate(x1):
-                out_file.write(str(c) + '\t' + str(y1[i]) + '\n')
+                out_file.write('{x}\t{y}\n'.format(x=c, y=(data[x][y][i]-data[x][y][-1])*scales[x][y]))
             out_file.write('&\n')
 
-            out_file.write('@target G1.S0\n@type xy\n')
-            for i, c in enumerate(x1):
-                out_file.write(str(c) + '\t' + str(y2[i]) + '\n')
-            out_file.write('&\n')
-
-            out_file.close()
-
+        out_file.close()

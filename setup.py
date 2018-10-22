eab7d1670d554231e2950d6ffce6621a5ac7435f
stur86 <simonesturniolo@gmail.com>
2018-10-22 15:14:53 +0100

    Recycled version number

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python
 
 from distutils.core import setup
+from castepconv import __vers_number__
 
 setup(name='castepconv',
-    version='1.0.1',
+    version=__vers_number__,
     packages = ['cconv'],
     scripts=['castepconv.py'],
     )
